47ff3cef4273f3f13e2fdbde56f98a4da3d2b206
Chan Lee and Meria Thomas <meria@>
2018-10-09 14:58:04 -0700

    update testing.

---
+++ spec/sudoku-spec.js
@@ -0,0 +1,21 @@
+import { Row, Board } from '../src/sudoku.js';
+
+
+describe('Sudoku' , function() {
+  var rowOne;
+  var rowTwo;
+  var rowThree;
+  var newBoard;
+
+  beforeEach(function() {
+    rowOne = [0,1,2];
+    rowTwo = [1,2,0];
+    rowThree = [2,0,1];
+    newBoard = new Board(rowOne, rowTwo, rowThree);
+  });
+
+  it('should check whether row has all numbers from 0-2', function(){
+    var checkerResult = newBoard.rowChecker();
+    expect(checkerResult).toEqual(true);
+  });
+});
