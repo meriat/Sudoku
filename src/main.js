edf0650d77c29c7d55dbf263d76197e0a13e799d
Chan Lee and Meria Thomas <meria@>
2018-10-09 16:45:53 -0700

    update front-end function.

--- src/main.js
+++ src/main.js
@@ -4,5 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-
+    $('#form').submit(function(event){
+      event.preventDefault();
+      var inputNumbers = $(".inputNumbers");
+      var allInputs = [];
+      var allInputsInRow = [];
+      $.each(inputNumbers, function(i, inputNumber) {
+        if (i % 3 == 0 && i != 0) {
+          allInputs.push(allInputsInRow);
+          allInputsInRow = [];
+          allInputsInRow.push(parseInt($(inputNumber).val()));
+        }
+        else if (i == inputNumbers.length-1) {
+          allInputsInRow.push(parseInt($(inputNumber).val()));
+          allInputs.push(allInputsInRow);
+        }
+        else {
+          allInputsInRow.push(parseInt($(inputNumber).val()));
+        }
+      });
+      console.log(allInputs);
+    });
 });
