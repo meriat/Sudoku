2baf5cda0ce3f2e3b3450ba95b0c0e4bf8a2bd45
Chan Lee and Meria Thomas <meria@>
2018-10-09 14:57:14 -0700

    add object for Board.

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,23 +1,70 @@
-var matrix =
-[ [0,1,2],
-  [1,2,0],
-  [2,0,1] ];
+// var matrix =
+// [ [0,1,2],
+//   [1,2,0],
+//   [2,0,1] ];
 
-var numberArray = [0,1,2];
+// export var Row = function(one, two, three) {
+//   var rowArray = [];
+//   rowArray.push(one);
+//   rowArray.push(two);
+//   rowArray.push(three);
+//   this.columns = rowArray.slice();
+// }
+// new Board([1,2,0],[2,1,0],[0,1,2])
 
-function colChecker(){
+export var Board = function (rowOne, rowTwo, rowThree)
+{
+  var tempArray= [];
+  tempArray.push(rowOne);
+  tempArray.push(rowTwo);
+  tempArray.push(rowThree);
+  this.matrix = tempArray.slice();
+}
+
+// var rowOne = new Row(0,1,2);
+// var rowTwo = new Row(1,2,0);
+// var rowThree = new Row(2,0,1);
+// var newBoard = new Board(rowOne, rowTwo, rowThree);
+
+
+export var numberArray = [0,1,2];
+
+
+Board.prototype.rowChecker = function () {
+
+  var matchedRow = 0;
+  for (var i=0; i< 3; i++){
+    var storedArray = [];
+    var tempMatrix = this.matrix;
+    storedArray = tempMatrix[i].slice();
+    if(storedArray.sort().join('') === numberArray.join(''))
+    {
+      matchedRow++;
+    }
+  }
+
+  if (matchedRow === 3) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+Board.prototype.colChecker = function (){
   var matchedCol = 0;
-  for (j=0; j< 3; j++)
+  for (var j=0; j< 3; j++)
   {
     var storedArray = [];
-    for (i=0; i< 3; i++) {
-      storedArray.push(matrix[i][j]);
+    for (var i=0; i< 3; i++) {
+      var tempMatrix = this.matrix;
+      storedArray.push(tempMatrix[i][j]);
     }
 
     if(storedArray.sort().join('') === numberArray.join('')) {
       matchedCol++;
     }
-
   }
 
   if(matchedCol === 3){
@@ -27,16 +74,13 @@ function colChecker(){
   }
 }
 
-function rowChecker(){
-  var matchedRow = 0;
-  for (i=0; i< 3; i++){
-    if(matrix[i].sort().join('') === numberArray.join(''))
-    {
-      matchedRow++;
-    }
-  }
 
-  if (matchedRow === 3) {
+Board.prototype.ultChecker = function () {
+
+  var rowResult = this.rowChecker();
+  var colResult = this.colChecker();
+
+  if(rowResult && colResult) {
     return true;
   }
   else {
